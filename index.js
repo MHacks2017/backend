5c65f31738c75accd82d73eab9cc93e5ae0511b6
Aaron Levin <awlevin@comcast.net>
2017-09-23 16:27:10 -0400

    post route added and functional but not yet implemented

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 require('dotenv').config()
-require('./intrepidapi.js')
+let api = require('./intrepidapi.js')
 const express = require('express')
 const app = express()
 
 let port = process.env.PORT
+let postTypes = ['Body.Door.FrontLeft', 'Body.Door.FrontRight', 'Body.Door.BackLeft', 'Body.Door.BackRight', 'Body.Window.FrontLeft', 'Body.Window.FrontRight', 'Body.Window.BackLeft', 'Body.Window.BackRight', 'Body.Liftgate', 'Body.FrontLiftgate', 'Body.Unlock', 'Body.Lock']
+
 
 
 app.get('/', function(req, res) {
   res.json({value: 'Hello World!'})
 })
+
+app.get('/alldata', function(req, res) {
+  api.intrepid_api_get_call()
+  .then( result => {
+    res.json(result)
+  })
+  .catch( err => {
+    console.log('endpoint /alldata caught error: ' + err)
+  })
+})
+
+
+// requires a request object with body.postType equal to a value on https://mhacks.intrepidcs.com/docs, and a value associated with postType
+app.post('/postdata', function(req, res) { 
+  api.intrepid_api_post_call()
+  .then (result => {
+    res.json(result)
+  })
+  .catch( err => {
+    console.log('endpoint /postdata caught error: ' + err)
+  })
+})
+
 
 app.listen(port, function() {
   console.log('Listening on port ' + port)
--- intrepidapi.js
+++ intrepidapi.js
@@ -3,7 +3,7 @@ var rp = require('request-promise')
 const util = require('util') // so we can print [Object object]
 
 
-var options = { 
+var getOptions = { 
   uri: "https://mhacks.intrepidcs.com/api/data",  
   headers: {
     Authorization: 'Bearer ' + process.env.INTREPID_PRIVATE_KEY
@@ -11,14 +11,20 @@ var options = {
   json: true
 }
 
-rp(options)
+module.exports.intrepid_api_get_call = function() {
+  return request(getOptions)
   .then( res => {
     console.log('received response with data: ' + util.inspect(res))
   })
   .catch( err => {
     console.log('api call failed')
   })
+}
 
+module.exports.intrepid_api_post_call = function() {
+  console.log('ERR: Not yet implemented.')
+  return Promise.resolve({value: "dummy"})
+}
 
 /*
 http.get(options, function(res) {
@@ -34,3 +40,6 @@ http.get(options, function(res) {
   console.log('Got error: ' + e.message)
 })
 */
+
+
+
